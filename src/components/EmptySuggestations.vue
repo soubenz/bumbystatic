1c57fe5fbf4e410271517ef56ee66d981eeee546
Soufiane Benzaoui <f>
2020-10-25 17:53:01 +0100

    Add emptySugesstations component

---
+++ src/components/EmptySuggestations.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center"
+    ><v-col cols="6" sm="6">
+      <v-icon center :size="180">mdi-alert-circle-outline</v-icon>
+
+      <p class="text-h4">No suggestions added yet</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
